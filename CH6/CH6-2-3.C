832cc660c17f9d402836c3256244107c0f3fefb2
anghanshrey <shreyanghan205@gmail.com>
2023-05-17 17:01:22 +0530

    CH6

---
+++ CH6/CH6-2-3.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+  int i=1,n;
+  clrscr();
+  printf("enter any number :");
+  scanf("%d",&n);
+  do{
+      if(i%2==0)
+	   {
+	    printf("%d\n",i);
+	   }
+      i++;
+     }while(i<=n);
+  getch();
+}
